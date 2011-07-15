c2221a08c3f83e5c03cd4994948bafd86f7d0ae3
Thomas <tomthecondor@free.fr>
2011-07-15 22:40:51 +0200

    first commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.data.validation.Required;
+import play.mvc.Controller;
+
+public class Application extends Controller {
+
+	public static void index() {
+		render();
+	}
+
+
+
+}
